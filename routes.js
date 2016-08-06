6787cc62e18cd99d571f199cf9c9156a934002ae
Luis Lopez <luishendrix92@gmail.com>
2016-08-05 23:32:08 -0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,14 +1,19 @@
-const Joi          = require('joi');
-const mongoose     = require('mongoose');
-const Schema       = mongoose.Schema;
-const randomString = require('./createhash');
-const hashLen  = 8; /* 8 chars long */
+const Joi        = require('joi');
+const mongoose   = require('mongoose');
+const Schema     = mongoose.Schema;
+const createHash = require('./createhash');
+const hashLen    = 8; /* 8 chars long */
+// Local machine? Set baseUrl to 'http://localhost:3000'
+// It's important that you don't add the slash at the end
+// or else, it will conflict with one of the routes
+const baseUrl    = 'http://your-domain.com';
+const urlPattern = /^https?:\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
 
 /* CREATING MONGOOSE SCHEMAS
- =====================================================================*/
+ ================================================*/
 
 const redirSchema = new Schema({
-  hash: String,
+  shortUrl: String,
   url: String,
   createdAt: Date
 });
@@ -23,7 +28,11 @@ module.exports = [
     method: 'GET',
     path:'/{hash}',
     handler(request, reply) {
-      Redir.findOne({ 'hash': request.params.hash }, (err, redir) => {
+      const query = {
+        'shortUrl': `${baseUrl}/${request.params.hash}`
+      };
+      
+      Redir.findOne(query, (err, redir) => {
         if (err) { return reply(err); }
         else if (redir) { reply().redirect(redir.url); }
         else { reply.file('views/404.html').code(404); }
@@ -31,24 +40,27 @@ module.exports = [
     }
   },
   {
-    method: 'POST',
-    path: '/new',
-    handler(request, reply) {
-      const newRedir = new Redir({
-        hash: randomString(hashLen),
-        url: request.payload.url,
-        createdAt: new Date()
-      });
-      
-      newRedir.save((err, redir) => {
-        if (err) { reply(err); } else { reply(redir); }
-      });
-    },
-    config: {
-      validate: {
-        payload: { url: Joi.string().regex(/^https?:\/\//).required() }
-      }
-    }
+   method: 'POST',
+   path: '/new',
+   handler(request, reply) {
+     const uniqueID = createHash(hashLen);
+     const newRedir = new Redir({
+       shortUrl: `${baseUrl}/${uniqueID}`,
+       url: request.payload.url,
+       createdAt: new Date()
+     });
+     
+     newRedir.save((err, redir) => {
+       if (err) { reply(err); } else { reply(redir); }
+     });
+   },
+   config: {
+     validate: {
+       payload: {
+         url: Joi.string().regex(urlPattern).required()
+       }
+     }
+   }
   },
   {
     method: 'GET',
